629294a51e76b3d2cd1def4f7328dd0cdc7d8170
Ryan Gauger <rgauger.dev@gmail.com>
2021-02-02 17:55:08 +1000

    Add DbUp deployment scripts
    Using DbUp because it keeps tracks of applied
    SQL scripts for database creates/migrations.

---
+++ src/Presentation/PncUniform.Shopping.UniformInventory.DbUp/DbUpSettings.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace PncUniform.Shopping.UniformInventory.DbUp
+{
+    public class DbUpSettings
+    {
+        [Option('c', "connectionString", Required = true, HelpText = "The PNC Database connection string.")]
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ src/Presentation/PncUniform.Shopping.UniformInventory.DbUp/Scripts/DeploymentScripts/001_CreateInitialTables.sql
@@ -0,0 +1,50 @@
+﻿CREATE TABLE [dbo].[Customers] (
+	CustomerId [INT] IDENTITY(1,1) NOT NULL,
+	CustomerName [NVARCHAR](100) NOT NULL,
+	CustomerEmail [NVARCHAR](150) NOT NULL,
+	CustomerMobile [INT] NOT NUll,
+
+	CONSTRAINT PK_Customers_CustomerId PRIMARY KEY (CustomerId)
+);
+
+CREATE TABLE [dbo].[Orders] (
+	OrderID [INT] IDENTITY (1,1) NOT NULL,
+	CustomerID [INT] NOT NULL,
+	OrderDate [DATE] NOT NULL, 
+	StudentName [CHAR] (50) NOT NULL,
+	ClassName [CHAR] (50) NOT NULL,
+	DeliveryOption [CHAR] (50) NOT NULL,
+	PaymentType [CHAR] (50) NOT NULL,
+	Paid [BIT] NOT NULL,
+
+	CONSTRAINT PK_Orders_OrderId PRIMARY KEY (OrderId),
+
+	CONSTRAINT FK_Orders_CustomerId FOREIGN KEY (CustomerId)
+		REFERENCES [dbo].[Customers](CustomerId)
+);
+
+CREATE TABLE [dbo].[OrderItem] (
+	OrderItemID [INT] IDENTITY (1,1) NOT NULL,
+	OrderId [INT] NOT NULL,
+	UniformID [INT] NOT NULL,
+	Quantity [INT] NOT NULL,
+
+	CONSTRAINT FK_OrderItem_OrderId FOREIGN KEY (OrderId)
+		REFERENCES [dbo].[Orders](OrderId),
+
+	CONSTRAINT FK_OrderItem_UniformId FOREIGN KEY (UniformId)
+		REFERENCES [dbo].[Customers](CustomerId)
+);
+
+CREATE TABLE [dbo].[Uniforms] (
+	UniformId [INT] IDENTITY (1,1) NOT NULL,
+	Description [NVARCHAR] (225) NOT NULL,
+	Size [NVARCHAR] (15) NOT NULL,
+	Price [DECIMAL] (2) NOT NULL,
+	StockLevel [INT] NOT NULL,
+	Campus [VARCHAR] (50) NOT NULL,
+	Barcode [VARCHAR] (20) NOT NULL,
+	VendorBarcode [VARCHAR] (20) Not Null,
+
+	CONSTRAINT FK_Uniforms_UniformId PRIMARY KEY (UniformId)
+);
