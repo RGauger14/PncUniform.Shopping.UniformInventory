7085a70315846468b2be875a31b25c1f03e0d6f3
Ryan Gauger <rgauger.dev@gmail.com>
2021-04-29 14:27:59 +1000

    Added Find all Uniforms Integration Test.

---
+++ src/Tests/PncUniform.Shopping.UniformInventory.Application.Tests/Uniforms/QueryTests/FindAllUnifromsIntergrationTest.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PncUniform.Shopping.UniformInventory.Application.Domain.Entities;
+using PncUniform.Shopping.UniformInventory.Application.Uniforms.Commands;
+using Xunit;
+
+namespace PncUniform.Shopping.UniformInventory.Application.Tests.Uniforms.QueryTests
+{
+    public class FindAllUnifromsIntergrationTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task CanSucsessfullyFindAllUniforms_WhenGivenValidData()
+        {
+            // Arrange
+            var client = _testServer.CreateClient();
+            var uniformsToFind = await CreateUniformsToFindForTestAsync(client, numberOfUniforms: 2);
+
+            // Act
+            // Then find all
+            var response = await client.GetAsync("api/uniform/findAll");
+
+            // Assert
+            // Then make sure each one is there
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var uniforms = JsonConvert.DeserializeObject<IEnumerable<Uniform>>(responseContent);
+            Assert.Equal(uniformsToFind.Count(), uniforms.Count());
+
+            var actualUniforms = uniforms.OrderBy(u => u.UniformId).ToList();
+            var expectedUniforms = uniformsToFind.OrderBy(u => u.UniformId).ToList();
+
+            for (int i = 0; i < actualUniforms.Count; i++)
+            {
+                var actualUniform = actualUniforms[i];
+                var expectedUniform = expectedUniforms[i];
+                Assert.Equal(expectedUniform.UniformId, actualUniform.UniformId);
+                Assert.Equal(expectedUniform.Description, actualUniform.Description);
+                Assert.Equal(expectedUniform.Size, actualUniform.Size);
+                Assert.Equal(expectedUniform.Price, actualUniform.Price);
+                Assert.Equal(expectedUniform.StockLevel, actualUniform.StockLevel);
+                Assert.Equal(expectedUniform.Campus, actualUniform.Campus);
+                Assert.Equal(expectedUniform.Barcode, actualUniform.Barcode);
+                Assert.Equal(expectedUniform.VendorBarcode, actualUniform.VendorBarcode);
+            }
+        }
+
+        /* [Fact]
+         public async Task CanSucsessfullyReturnValidationError_WhenGivenInvalidData()
+         {
+             // arrange (set up test)
+
+             // Act (calling item test)
+
+             // Assert (ensure corrent response)
+             // assert error message
+             // assert http status code response (bad request)
+         }*/
+
+        private async Task<IEnumerable<Uniform>> CreateUniformsToFindForTestAsync(HttpClient client, int numberOfUniforms)
+        {
+            var uniformCommands = new List<CreateUniformCommand>();
+            var sizes = new string[] { "Medium", "L", "XXL" };
+            var campuses = new string[] { "Primary", "Middle", "Senior" };
+
+            for (int i = 0; i < numberOfUniforms; i++)
+            {
+                var description = $"description{i}";
+                var size = sizes[i % sizes.Length];
+                var price = 44.50m;
+                var stockLevel = 16;
+                var campus = campuses[i % campuses.Length];
+                var barcode = $"{i % 10}{i % 3}{i % 10}{i % 6}451657773";
+                var vendorBarcode = $"{i % 3}{i % 10}{i % 6}{i % 10}456157123";
+
+                var createUniformCommand = new CreateUniformCommand
+                {
+                    Description = description,
+                    Size = size,
+                    Price = price,
+                    StockLevel = stockLevel,
+                    Campus = campus,
+                    Barcode = barcode,
+                    VendorBarcode = vendorBarcode
+                };
+
+                uniformCommands.Add(createUniformCommand);
+            }
+
+            var uniforms = new List<Uniform>();
+
+            foreach (var uniformCommand in uniformCommands)
+            {
+                var uniform = await CreateUniformForTestAsync(client, uniformCommand);
+                uniforms.Add(uniform);
+            }
+
+            return uniforms;
+        }
+
+        private async Task<Uniform> CreateUniformForTestAsync(HttpClient client, CreateUniformCommand uniformCommand)
+        {
+            var requestContent = new StringContent(JsonConvert.SerializeObject(uniformCommand), Encoding.UTF8, MediaTypeNames.Application.Json);
+            var responseMessage = await client.PostAsync("api/uniform/create", requestContent);
+            var responceContent = await responseMessage.Content.ReadAsStringAsync();
+            var createdUniform = JsonConvert.DeserializeObject<Uniform>(responceContent);
+
+            return createdUniform;
+        }
+    }
+}
